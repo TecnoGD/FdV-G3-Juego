50df531596d694d0a63bd57d115e58543898c46e
zzzzzz913 <zinth520@gmail.com>
2025-11-28 02:31:45 +0100

    Menu pausa

--- Assets/Codigo/Scripts/Jugador.cs
+++ Assets/Codigo/Scripts/Jugador.cs
@@ -60,7 +60,8 @@ public class Jugador : MonoBehaviour
     {
         bool hablando = SistemaDialogo.instance != null && SistemaDialogo.instance.enDialogo;
         //string escena = SceneManager.GetActiveScene().name;
-        if (!GLOBAL.enCombate && !hablando && !MenuPausa.enPausa) // si no estamos ni en combate ni en dialogo ni en pausa
+        // si no estamos ni en combate ni en dialogo ni en pausa
+        if (!GLOBAL.enCombate && !hablando && !MenuPausa.enPausa) 
         {
             ControlMovimiento();
             sistemaInteraccion.DetectarInteraccion();
